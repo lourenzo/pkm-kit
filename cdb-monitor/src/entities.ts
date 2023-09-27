dc1915a6df4f800caad3bd04d9bf92f36b3248d5
Lourenzo Ferreira <lourenzo@gmail.com>
2023-09-26 22:34:51 -0300

    refactor: rewrite entities translating terms to English

--- cdb-monitor/src/entities.ts
+++ cdb-monitor/src/entities.ts
@@ -1,25 +1,45 @@
 /**
- * Contabilidade - visualização
+ * Investment Accounting Monitor
+ * - Data Entities
  */
-// Saída
-type Retirada = {
-  valor: number;
-  irpf: number;
-  iof: number;
-  taxes: number;
+type Tax = {
+  type: 'irpf' | 'iof';
+  amount: number;
+  percentile?: number;
 };
 
-// Entrada
-type Deposito = {
-  valor: number;
+type Withdrawal = {
+  netAmount: number;
+  totalAmount: number;
+  taxesAmount?: number;
+  taxes: Tax[];
 };
+
+type Deposit = {
+  amount: number;
+};
+
+type ReturnOnInvestment = {
+  amount: number;
+  percentile: number;
+}; // TODO: refine this part to cover different CDB   product and operations
 
 // Registro com soma
 type InvestmentRecord = {
-  valorEfetivo: number;
-  deposito?: Deposito;
-  retirada?: Retirada;
+  // Metadata
+  effectiveValue: number;
+  referenceDate: Date; // TODO: look for alternative to Day.js in typescript
+
+  // Referenced entities
+  // TODO: is there a pattern for referencing entities in TS?
+  deposit?: Deposit;
+  withdrawal?: Withdrawal;
+  returnOnInvestment: ReturnOnInvestment;
 };
 
-// Movimentação do Período
-// -> Nota: incluir
+// Movimentação e Rendimentos do Período - Balanço diário
+// -> Nota: incluir data início, e exibir apenas dias bancários
+// TODO: procurar lista de dias bancários
+
+// TODO: study bookkeeping / accounting terms in English: https://en.wikipedia.org/wiki/Bookkeeping
+// @see https://www.investopedia.com/investing-4427685
